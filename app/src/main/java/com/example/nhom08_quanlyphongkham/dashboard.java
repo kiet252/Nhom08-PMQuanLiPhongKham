cf6c9306310363eb397831aa864a528242421c39
01nhak01 <nhak010106@gmail.com>
2026-03-24 08:59:47 +0700

    HomeFragment cho 3 roll

--- app/src/main/java/com/example/nhom08_quanlyphongkham/dashboard.java
+++ app/src/main/java/com/example/nhom08_quanlyphongkham/dashboard.java
@@ -1,18 +1,14 @@
 package com.example.nhom08_quanlyphongkham;
 
-import static androidx.core.content.ContextCompat.startActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import com.example.nhom08_quanlyphongkham.uilogin.UserProfile;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import dashboard_fragment.HomeFragment;
+import dashboard_fragment.HomeFragment_admin;
 
 public class dashboard extends AppCompatActivity {
 
@@ -25,8 +21,8 @@ public class dashboard extends AppCompatActivity {
             return;
         }
         setContentView(R.layout.dashboard);
-        HomeFragment homeFragment = new HomeFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
+        HomeFragment_admin homeFragmentAdmin = new HomeFragment_admin();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragmentAdmin).commit();
         //UserProfile profile = (UserProfile) getIntent().getSerializableExtra("name");
         //TextView txtWelcome = findViewById(R.id.txtWelcome);
 
@@ -46,7 +42,7 @@ public class dashboard extends AppCompatActivity {
             int itemId = item.getItemId(); // Đây là cách xác định nút nào đang được chạm vào
 
             if (itemId == R.id.nav_home) {
-                selectedFragment = new HomeFragment();
+                selectedFragment = new HomeFragment_admin();
             } else if (itemId == R.id.nav_notifications) { // Đổi từ doctor sang notification ở đây
                 selectedFragment = new NotificationFragment();
             } else if (itemId == R.id.nav_account) {
