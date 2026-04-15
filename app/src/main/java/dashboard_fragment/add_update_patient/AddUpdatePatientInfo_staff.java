b1eadf2d65aaf9033ca041d7358d137007682e07
khanhhbdt123 <khanhhbdt123@gmail.com>
2026-04-15 14:37:49 +0700

    sửa giao diện để chuyển tab Tạo mới và Cập nhật trông mượt mà hơn và sửa lại kiểu Button thành Material Button trong file AddUpdatePatientInfo_staff.java

--- app/src/main/java/dashboard_fragment/add_update_patient/AddUpdatePatientInfo_staff.java
+++ app/src/main/java/dashboard_fragment/add_update_patient/AddUpdatePatientInfo_staff.java
@@ -1,7 +1,10 @@
 package dashboard_fragment.add_update_patient;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -13,13 +16,14 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.nhom08_quanlyphongkham.R;
+import com.google.android.material.button.MaterialButton;
 
 import dashboard_fragment.add_update_patient.add_patient_logic.CreatePatientFragment;
 import dashboard_fragment.add_update_patient.update_patient_logic.UpdatePatientFragment;
 
 public class AddUpdatePatientInfo_staff extends AppCompatActivity {
 
-    private Button BtnCreatePatient, BtnUpdatePatient, BtnSavePatientInfo, BtnExitAddCreatePatient;
+    private MaterialButton BtnCreatePatient, BtnUpdatePatient, BtnSavePatientInfo, BtnExitAddCreatePatient;
     private ImageButton BtnBackAddUpdatePatient;
     private String currentToken;
     private CreatePatientFragment CurrentCreatePatientFragment;
@@ -63,6 +67,7 @@ public class AddUpdatePatientInfo_staff extends AppCompatActivity {
     }
 
     private void CreateNewPatientReplaceFragment() {
+        updateTabUI(true);
         CurrentCreatePatientFragment = new CreatePatientFragment(currentToken);
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.patientFragmentContainer, CurrentCreatePatientFragment)
@@ -70,6 +75,7 @@ public class AddUpdatePatientInfo_staff extends AppCompatActivity {
     }
 
     private void UpdatePatientReplaceFragment() {
+        updateTabUI(false);
         CurrentUpdatePatientFragment = new UpdatePatientFragment(currentToken);
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.patientFragmentContainer, CurrentUpdatePatientFragment)
@@ -85,7 +91,32 @@ public class AddUpdatePatientInfo_staff extends AppCompatActivity {
             ((UpdatePatientFragment) currentVisibleFrag).submitPatientData();
         }
     }
+    private void updateTabUI(boolean isCreateSelected) {
 
+        int colorWhite = Color.WHITE;
+        int colorGray = Color.parseColor("#757575");
+        if (isCreateSelected) {
+
+            BtnCreatePatient.setBackgroundResource(R.drawable.bg_tab_selected);
+            BtnCreatePatient.setTextColor(colorWhite);
+            BtnCreatePatient.setIconTintResource(android.R.color.white);
+
+
+            BtnUpdatePatient.setBackgroundResource(R.drawable.bg_tab_unselected);
+            BtnUpdatePatient.setTextColor(colorGray);
+            BtnUpdatePatient.setIconTintResource(android.R.color.darker_gray);
+        } else {
+
+            BtnUpdatePatient.setBackgroundResource(R.drawable.bg_tab_selected);
+            BtnUpdatePatient.setTextColor(colorWhite);
+            BtnUpdatePatient.setIconTintResource(android.R.color.white);
+
+
+            BtnCreatePatient.setBackgroundResource(R.drawable.bg_tab_unselected);
+            BtnCreatePatient.setTextColor(colorGray);
+            BtnCreatePatient.setIconTintResource(android.R.color.darker_gray);
+        }
+    }
     private void backToPreviousActivity() {
         finish();
     }
