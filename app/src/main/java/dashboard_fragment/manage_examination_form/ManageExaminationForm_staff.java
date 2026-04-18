1b11ecd730f9f27dfd8493f20c310aeaafb9730c
khanhhbdt123 <khanhhbdt123@gmail.com>
2026-04-18 17:08:58 +0700

    xong giao diện quản lý phiếu khám, gồm nhiều thứ như khi bấm vào sắp xếp thì ra sort_examination_date_menu, khi bấm lọc thì ra filter_doctor_dialog và v.v...

---
+++ app/src/main/java/dashboard_fragment/manage_examination_form/ManageExaminationForm_staff.java
@@ -0,0 +1,26 @@
+package dashboard_fragment.manage_examination_form;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.nhom08_quanlyphongkham.R;
+
+public class ManageExaminationForm_staff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.staff_manage_examination_form);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
