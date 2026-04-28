93d71056fd4aaca6401140744d0ad28c3684e2b2
khanhhbdt123 <khanhhbdt123@gmail.com>
2026-04-28 21:41:58 +0700

    giao diện chỉnh sửa hồ sơ

---
+++ app/src/main/java/dashboard_fragment/edit_profile/EditProfile.java
@@ -0,0 +1,26 @@
+package dashboard_fragment.edit_profile;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.nhom08_quanlyphongkham.R;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.edit_profile);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_edit_profile), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
