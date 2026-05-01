e3bc02afc2e9128c48c561f66c9af7769e52cf51
01nhak01 <nhak010106@gmail.com>
2026-05-01 10:34:07 +0700

    -Thêm thư viện để đổ dữ liệu thong_bao, thong_bao_fragment_admin.

---
+++ app/src/main/java/com/example/nhom08_quanlyphongkham/uilogin/ThongBao.java
@@ -0,0 +1,27 @@
+package com.example.nhom08_quanlyphongkham.uilogin;
+
+import com.example.nhom08_quanlyphongkham.UserProfile;
+
+public class ThongBao
+{
+    private int id;
+    private String tieu_de;
+    private String noi_dung;
+
+    public ThongBao(String tieu_de, String noi_dung)
+    {
+        this.tieu_de = tieu_de;
+        this.noi_dung = noi_dung;
+    }
+
+    public String getTieu_de()
+    {
+        return tieu_de;
+    }
+
+    public String getNoi_dung()
+    {
+        return noi_dung;
+    }
+    public int getId() { return id; }
+}
