c0248046c93d42a221c0db82cf255f03aa32518d
01nhak01 <nhak010106@gmail.com>
2026-05-01 10:32:36 +0700

    -Thêm để đổ dữ liệu thong_bao, thong_bao_fragment_admin.

--- app/src/main/java/com/example/nhom08_quanlyphongkham/uilogin/AuthApiService.java
+++ app/src/main/java/com/example/nhom08_quanlyphongkham/uilogin/AuthApiService.java
@@ -7,6 +7,9 @@ import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.GET;
+import retrofit2.http.DELETE;
+import retrofit2.http.Query;
 
 public interface AuthApiService {
     @Headers("Content-Type: application/json")
@@ -15,11 +18,52 @@ public interface AuthApiService {
             @Header("apikey") String apiKey,
             @Body LoginRequest request
     );
-
     @PUT("auth/v1/user")
     Call<LoginResponse> updatePassword(
             @Header("apikey") String apiKey,
             @Header("Authorization") String authorization,
             @Body UpdatePasswordRequest request
+    );
+    @GET("rest/v1/thong_bao?select=*&order=id.asc")
+    Call<java.util.List<ThongBao>> layDanhSachThongBao(
+            @Header("apikey") String apiKey,
+            @Header("Authorization") String authorization
+    );
+    @POST("rest/v1/thong_bao")
+    @Headers({
+            "Content-Type: application/json",
+            "Prefer: return=representation"
+    })
+    Call<java.util.List<ThongBao>> themThongBao(
+            @Header("apikey") String apiKey,
+            @Header("Authorization") String authorization,
+            @Body ThongBao thongBao
+    );
+    @GET("rest/v1/thong_bao_fragment_admin?select=*&order=id.asc")
+    Call<java.util.List<ThongBao>> layDanhSachThongBaoAdmin(
+            @Header("apikey") String apiKey,
+            @Header("Authorization") String authorization
+    );
+    @POST("rest/v1/thong_bao_fragment_admin")
+    @Headers({
+            "Content-Type: application/json",
+            "Prefer: return=representation"
+    })
+    Call<java.util.List<ThongBao>> themThongBaoAdmin(
+            @Header("apikey") String apiKey,
+            @Header("Authorization") String authorization,
+            @Body ThongBao thongBao
+    );
+    @DELETE("rest/v1/thong_bao")
+    Call<Void> xoaThongBaoAdmin(
+            @Header("apikey") String apiKey,
+            @Header("Authorization") String authorization,
+            @Query("id") String queryId // id=eq.1
+    );
+    @DELETE("rest/v1/thong_bao_fragment_admin")
+    Call<Void> xoaThongBao(
+            @Header("apikey") String apiKey,
+            @Header("Authorization") String authorization,
+            @Query("id") String queryId // id=eq.1
     );
 }
