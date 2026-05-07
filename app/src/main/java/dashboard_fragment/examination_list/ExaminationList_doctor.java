f0607c30cbcef96eed08c1bd79fcd740b7f4857d
khanhhbdt123 <khanhhbdt123@gmail.com>
2026-05-07 20:51:24 +0700

    sơ sơ qua giao diện danh sách phiếu khám và cả đống item, sửa lại 1 tí giao diện home của bác sĩ

---
+++ app/src/main/java/dashboard_fragment/examination_list/ExaminationList_doctor.java
@@ -0,0 +1,26 @@
+package dashboard_fragment.examination_list;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.nhom08_quanlyphongkham.R;
+
+public class ExaminationList_doctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.doctor_examination_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainDoctorExaminationList), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
