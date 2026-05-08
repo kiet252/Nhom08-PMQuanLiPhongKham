4cc8ee6aa4854848d4928d2ffc916f7fd99a2b44
01nhak01 <nhak010106@gmail.com>
2026-05-08 17:28:50 +0700

    -Thêm biến chức vụ.

--- app/src/main/java/com/example/nhom08_quanlyphongkham/uilogin/LoginRequest.java
+++ app/src/main/java/com/example/nhom08_quanlyphongkham/uilogin/LoginRequest.java
@@ -2,6 +2,7 @@ package com.example.nhom08_quanlyphongkham.uilogin;
 public class LoginRequest {
     private String email;
     private String password;
+    private String chucvu;
 
     public LoginRequest(String email, String password) {
         this.email = email;
@@ -14,5 +15,13 @@ public class LoginRequest {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getChuc_vu() {
+        return chucvu;
+    }
+
+    public void setChuc_vu(String chuc_vu) {
+        this.chucvu = chuc_vu;
     }
 }
